f8133536e7d6355168c845ccc6d27c39f3571b01
Ahmed Murtaza <beyondmy_destiny@hotmail.com>
2018-04-23 05:47:21 -0400

    HOC in progress

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import ReactForm from './1-React-form';
 
-const App = () => <ReactForm />
+// const tableStyles = {
+//   border:'1px solid red',
+//   padding:'5px',
+//   fontFamily:'verdana',
+//   fontSize:'14px',
+//   textAlign:'left'
+// }
+
+const App = () => {
+  return(
+    <ReactForm />
+    // <table style={tableStyles}>
+    //   <thead>
+    //     <tr>
+    //       <th>Topics</th>
+    //       <th>Examples</th>
+    //     </tr>      
+    //   </thead>
+    //   <tbody>
+    //     <tr>
+    //       <td>Forms</td>
+    //       <td><a href="" style={{color:'green'}}>Click this Link</a></td>
+    //     </tr>
+    //   </tbody>
+    // </table>
+  );
+}
 
 render(<App />, document.getElementById("root"));
